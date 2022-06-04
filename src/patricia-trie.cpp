031805607d6225ba60bc177d18d9989be9fff0c2
Vinícius Costa <vinicius.lcosta@b2wdigital.com>
2022-06-04 11:49:04 -0300

    metodo de delete

--- src/patricia-trie.cpp
+++ src/patricia-trie.cpp
@@ -12,18 +12,18 @@ void PatriciaTree::addElement(string element)
 int matchingConsecutiveChars(string word, PatriciaNode *currentNode)
 {
     int matches = 0;
-    int minLength = 0;
+    int minlength = 0;
 
     // see which string is smaller and save it's lenght
     // when cycling throught the two strings we won't go any further than that
     if (currentNode->stringData.length() >= word.length())
-        minLength = word.length();
+        minlength = word.length();
     else if (currentNode->stringData.length() < word.length())
-        minLength = currentNode->stringData.length();
+        minlength = currentNode->stringData.length();
 
-    if (minLength > 0)
+    if (minlength > 0)
         // go throught the two streams
-        for (int i = 0; i < minLength; i++)
+        for (int i = 0; i < minlength; i++)
         {
             // if two characters at the same position have the same value we have one more match
             if (word[i] == currentNode->stringData[i])
@@ -60,7 +60,7 @@ void PatriciaTree::addRecursive(string wordPart, PatriciaNode *currentNode)
                 addRecursive(newWordPart, child);
             }
         }
-        // foreach (var child in curNode.childList)
+        // foreach (auto child in curNode.childList)
         //     if (child.stringData.StartsWith(newWordPart[0].ToString()))
         //     {
         //         inserted = true;
@@ -78,33 +78,81 @@ void PatriciaTree::addRecursive(string wordPart, PatriciaNode *currentNode)
         // matching characters(common characters)
         // the other is the node that has it's label extracted from the current word part minus the string
         // of matching characters
-        string commonRoot = wordPart.Substring(0, matches);
-        string branchPreviousLabel = currentNode.stringData.Substring(matches, currentNode.stringData.length() - matches);
-        string branchNewLabel = wordPart.Substring(matches, wordPart.length() - matches);
+        string commonRoot = wordPart.substr(0, matches);
+        string branchPreviousstringData = currentNode->stringData.substr(matches, currentNode->stringData.length() - matches);
+        string branchNewstringData = wordPart.substr(matches, wordPart.length() - matches);
 
-        currentNode.stringData = commonRoot;
+        currentNode->stringData = commonRoot;
 
-        auto newNodePreviousLabel = new Node(branchPreviousLabel);
-        newNodePreviousLabel.childList.AddRange(currentNode.childList);
+        auto newNodePreviousstringData = new PatriciaNode(branchPreviousstringData);
+        newNodePreviousstringData->childList->insert(newNodePreviousstringData->childList->end(),
+                                                     currentNode->childList->begin(), currentNode->childList->end());
 
-        currentNode.childList.Clear();
-        currentNode.childList.Add(newNodePreviousLabel);
+        currentNode->childList->clear(); // TODO DELETE ?
+        currentNode->childList->push_back(newNodePreviousstringData);
 
-        auto newNodeNewLabel = new Node(branchNewLabel);
-        currentNode.childList.Add(newNodeNewLabel);
+        auto newNodeNewstringData = new PatriciaNode(branchNewstringData);
+        currentNode->childList->push_back(newNodeNewstringData);
     }
-    else if (matches == currentNode.stringData.length())
+    else if (matches == currentNode->stringData.length())
     {
         // in this case we don't do anything because the word is already added
     }
-    else if (matches > currentNode.stringData.length())
+    else if (matches > currentNode->stringData.length())
     {
         // add the current word part minus the common characters after the current node
-        string newNodeLabel = currentNode.stringData.Substring(currentNode.stringData.length(), wordPart.length());
-        var newNode = new Node(newNodeLabel);
-        currentNode.childList.Add(newNode);
+        string newNodestringData = currentNode->stringData.substr(currentNode->stringData.length(), wordPart.length());
+        auto newNode = new PatriciaNode(newNodestringData);
+        currentNode->childList->push_back(newNode);
     }
 }
+
+/// <summary>
+/// Delete: Delete a string from the tree. First, we delete the corresponding leaf.
+/// Then, if its parent only has one child remaining, we delete the parent and merge the two incident edges.
+/// </summary>
+/// <param name="label"></param>
+void PatriciaTree::deleteElement(string label)
+{
+    deleteRecursive(label, root);
+}
+
+/// <summary>
+/// delete a word from the tree means delete the last leaf that makes up the stored word
+/// </summary>
+/// <param name="label"></param>
+/// <param name="curNode"></param>
+void PatriciaTree::deleteRecursive(string wordPart, PatriciaNode *currentNode)
+{
+    auto matches = matchingConsecutiveChars(wordPart, currentNode);
+
+    if ((matches == 0) || (currentNode == root) ||
+        ((matches > 0) && (matches < wordPart.length()) && (matches >= currentNode->stringData.length())))
+    {
+
+        auto newStringData = wordPart.substr(matches, wordPart.length() - matches);
+        for (int i = 0; i < currentNode->childList->size(); i++)
+        {
+            PatriciaNode *child = currentNode->childList->at(i);
+
+            if (child->stringData.rfind(newStringData, 0) == 0)
+            {
+                if (newStringData == child->stringData)
+                {
+                    if (child->childList->size() == 0)
+                    {
+                        auto it = currentNode->childList->begin();
+                        it += i;
+                        currentNode->childList->erase(it); // todo check
+                        return;
+                    }
+                }
+                deleteRecursive(newStringData, child);
+            }
+        }
+    }
+}
+
 PatriciaTree::PatriciaTree()
 {
     root = nullptr;
@@ -112,4 +160,8 @@ PatriciaTree::PatriciaTree()
 PatriciaTree::PatriciaTree(string hash, string stringData)
 {
     root = new PatriciaNode(stringData);
+}
+PatriciaTree::~PatriciaTree()
+{
+    delete root;
 }
--- src/patricia-trie.hpp
+++ src/patricia-trie.hpp
@@ -18,21 +18,31 @@ public:
         stringData = l;
         childList = new std::vector<PatriciaNode *>();
     }
+    ~PatriciaNode()
+    {
+        stringData = "";
+        for (int i = 0; i < childList->size(); i++)
+        {
+            delete childList->at(i);
+        }
+        childList->clear();
+        delete childList;
+    }
 };
 class PatriciaTree
 {
 private:
     PatriciaNode *root;
     void addRecursive(string wordPart, PatriciaNode *currentNode);
-    // std::map<char, Node *> rootMap;
+    void deleteRecursive(string wordPart, PatriciaNode *currentNode);
 
 public:
     void addElement(std::string element); // transaction
-    PatriciaNode *getElement();
-    PatriciaNode *removeElement(); //
+
     PatriciaTree();
     PatriciaTree(std::string hash, std::string data);
     ~PatriciaTree();
+    void deleteElement(string label);
     // bool bit();
     // bool search(int i);
     // Node *search(Node *node);
